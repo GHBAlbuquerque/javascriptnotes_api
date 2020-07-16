0b0394be7c7088b2a13c86bad17ff6ce24be1808
Giovanna Albuquerque <ghb.albuquerque@gmail.com>
2020-07-16 11:05:06 -0300

    first login

--- config/database.js
+++ config/database.js
@@ -1,12 +1,12 @@
-const mongoose = require ('mongoose');
+var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-require ('dotenv').config();
-const MONGO_URL = process.env.MONGO_URL
+require('dotenv').config();
+const MONGO_URL = process.env.MONGO_URL;
 
-mongoose.connect (MONGO_URL, {
+mongoose.connect(MONGO_URL, {
     useNewUrlParser: true, //para usar as novas ferramentas do mongoose
     useUnifiedTopology: true, //indexação dos conteudos
     useCreateIndex: true //tb tem a ver com indexação. para fazer buscas no mongo
-}).then(() => console.log('Connection succesful')) // para ele me dar uma mensagem quando conectar
-.catch ((error) => console.log(error)) //para mostrar um erro se nao conectar
+}).then(() => console.log('connection succesful')) // para ele me dar uma mensagem quando conectar
+    .catch((err) => console.error(err));  //para mostrar um erro se nao conectar
